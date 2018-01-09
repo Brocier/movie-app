337d18a642275111e185700b8e358457b567579a
Brocier <jschoe04@gmail.com>
2018-01-09 16:05:42 -0500

    broke apart movie and movielist functionality

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 import Movie from './components/Movie.js'
+import MoviesList from './components/MoviesList.js'
+import './App.css';
+
 
 class App extends Component {
   render() {
+
+    const movies = [
+      { title: 'A Movie', rating: 'PG-13', description: 'this is a movie' },
+      { title: 'Another Movie', rating: 'R', description: 'this is another movie' },
+      { title: 'A Third Movie', rating: 'PG', description: 'this is yet another movie' },
+    ]
+
     return (
-      <div>
-        <Movie />
-      </div>
+      <MoviesList movies={movies} />
     );
   }
 }
