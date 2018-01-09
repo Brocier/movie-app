97aa1ae22b2feedcb67c4e85bd9872993f54bcbb
Brocier <jschoe04@gmail.com>
2018-01-09 15:00:40 -0500

    adding reviews

---
+++ src/components/Review.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Review extends Component {
+  render() {
+    return (
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eos reiciendis ad quas sed quos aliquid error, ducimus, enim doloremque esse delectus cupiditate deleniti quo, blanditiis iusto expedita? Voluptatem, nesciunt laboriosam?
+         </div>
+    );
+  }
+}
+
+export default Review
